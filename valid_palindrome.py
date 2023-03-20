95b102ad71ed1c8b14ae7bd897fb55420ee41035
souvikghosh <Sayali1@>
2023-03-20 14:29:06 -0700

    Valid Palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,56 @@
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers. Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+'''
+
+# Step 1 - remove alphanumeric characters
+# Step 2 - convert all characters to lowercase and the compare
+# ord() to get ascii value
+
+# cheating solution
+# def isPlaindrome(s):
+#     newStr = ""
+
+#     for c in s:
+#         if c.isalnum():
+#             newStr += c.lower()
+    
+#     return newStr == newStr[::-1]
+
+class Solution:
+
+    def isPlaindrome(self, s):
+        # 2 pointers left and right
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        
+        return True
+
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
