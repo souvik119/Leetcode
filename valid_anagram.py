8d3b88bb06a3293c85f49de9238a700d0d8ec987
souvikghosh <Sayali1@>
2023-03-21 17:24:25 -0700

    completed valid anagram

--- valid_anagram.py
+++ valid_anagram.py
@@ -11,3 +11,24 @@ Input: s = "rat", t = "car"
 Output: false
 '''
 
+def is_anagram(s, t):
+
+    #check length
+    if len(s) != len(t):
+        return False
+    
+    mapS, mapT = {}, {}
+
+    # create the hashmap of S and T
+    for i in range(len(s)):
+        # get sets default value if key does not exist        
+        mapS[s[i]] = 1 + mapS.get(s[i], 0)
+        mapT[t[i]] = 1 + mapT.get(t[i], 0)
+
+    # compare the hashmaps
+    for key in mapS:
+        # get to avoid key error if key does not exist
+        if mapS[key] != mapT.get(key, 0):
+            return False
+        
+    return True
