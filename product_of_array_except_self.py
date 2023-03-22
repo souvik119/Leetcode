c7b7ef30c7c3af757c95a664dbaef71569c17f27
souvikghosh <Sayali1@>
2023-03-21 22:49:30 -0700

    product of all numbers except self

---
+++ product_of_array_except_self.py
@@ -0,0 +1,33 @@
+'''
+Given an integer array nums, return an array answer such that 
+answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+'''
+
+def prod_except_self(nums):
+
+    # defining result array
+    res = [1] * len(nums)
+
+    # build prefix numbers
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    # build postfix numbers
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+
+    return res
